d66ce948e645052bde5e70323e6278b8bd6202e4
gogofishman <gogofishman@qq.com>
2023-10-02 00:38:18 +0800

    更新到1.0.4

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         QQ空间说说删除脚本
 // @namespace    none
-// @version      1.0.3
-// @description  用于一键删除QQ空间的说说，附带空间简约美化
+// @version      1.0.4
+// @description  用于一键删除QQ空间的说说，附带空间简约风美化
 // @author       gogofishman
 // @license      MIT
 // @include      *://user.qzone.qq.com/*
@@ -15,8 +15,11 @@ var completed = 0;
 // 剩余数量
 var number = '0';
 
+
 (function () {
     'use strict';
+
+    Print('QQ空间说说删除脚本');
 
     //添加删除面板
     window.onload = AddDiv;
@@ -24,30 +27,44 @@ var number = '0';
 
 //添加控制面板
 function AddDiv() {
-    Beautify();
-
     let newDiv = document.createElement('div');
-    newDiv.id = 'ShuoShuoDelete';
-
-    //样式
-    let style = newDiv.style;
-    style.width = '200px';
-    style.height = '200px';
-    style.position = 'fixed';
-    style.top = '41px';
-    style.right = '0';
-    style.zIndex = '99999';
-    style.padding = '10px';
-    style.backgroundColor = 'white';
-    style.backgroundImage =
-        'url(https://p.sda1.dev/13/3f9eb408f25616eb024b972f7fd1a70d/忍野忍.png)';
-    style.backgroundRepeat = 'no-repeat';
-    style.backgroundSize = 'cover';
-    style.boxShadow = '0px 2px 5px rgba(0, 0, 0, 0.5)';
+    newDiv.id = 'ShuoShuoDeleteMain';
 
     //内容
     newDiv.innerHTML = `
 	<style>
+	#ShuoShuoDeleteMain{
+        width: 200px;
+        height: 200px;
+        position: fixed;
+        top: 41px;
+        right: 0;
+        z-index: 499;
+        padding: 10px;
+        background: white url(https://p.sda1.dev/13/3f9eb408f25616eb024b972f7fd1a70d/忍野忍.png) no-repeat;
+        background-size: cover;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
+        transition: transform 0.5s ease;
+	}
+	.moving#ShuoShuoDeleteMain {
+    	transform: translateY(-180px) translateX(175px);
+    	box-shadow:0 0 0;
+    	background: rgba(255,255,255,0);
+	}
+	
+	#ShuoShuoDelete_open{
+	    width: 18px;
+        height: 18px;
+        background-color: white;
+        border: 1px solid #777;
+        border-radius: 50%;
+        padding:5px;
+        cursor: pointer; /* 鼠标指针样式为手型 */
+	}
+    #ShuoShuoDelete_open:active{
+        background-color: darkgray;
+    }
+	
 	.shuoshuoDelete * {
 		margin-top: 5px;
 		margin-bottom: 5px;
@@ -55,45 +72,72 @@ function AddDiv() {
 	}
 	</style>
 	<div class="shuoshuoDelete" style="margin: 0 auto; text-align: center">
-	<h1 style="font-size: 17px; margin-bottom: 10px">QQ空间说说删除脚本</h1>
-	<h2>删除顺序：</h2>
-	<input type="radio" name="order" value="正序" checked />正序
-	<input type="radio" name="order" value="倒序" />倒序
-	<br />
-	<div style="margin-top: 20px">
-		<button id="shuoshuoDelete_start" style="padding: 5px">启动</button>
-		<button id="shuoshuoDelete_end" style="padding: 5px">停止</button>
+        <h1 style="font-size: 17px; margin-bottom: 10px">QQ空间说说删除脚本</h1>
+        <h2>删除顺序：</h2>
+        <input type="radio" name="order" value="正序" checked />正序
+        <input type="radio" name="order" value="倒序" />倒序
+        <br />
+        <div style="margin-top: 20px">
+            <button id="shuoshuoDelete_start" style="padding: 5px">启动</button>
+            <button id="shuoshuoDelete_end" style="padding: 5px">停止</button>
+        </div>
+        <p id="shuoshuoDelete_state" style="color: red">未运行</p>
 	</div>
-	<p id="shuoshuoDelete_state" style="color: red">未运行</p>
-	</div>
+    <div title="说说删除" id="ShuoShuoDelete_open">del</div>
   `;
 
     let div = document.getElementById('layBackground');
     if (div !== null) {
         div.appendChild(newDiv);
 
+        //添加打开隐藏
+        document.getElementById('ShuoShuoDelete_open').addEventListener('click', openMain);
+        document.getElementById('ShuoShuoDelete_open').click();
+
         //添加启动停止
         document
             .getElementById('shuoshuoDelete_start')
             .addEventListener('click', start);
         document.getElementById('shuoshuoDelete_end').addEventListener('click', end);
+
+        Beautify();
     }
 }
 
 //空间界面美化
-function Beautify() {
-    Print('美化空间');
+async function Beautify() {
     //移除flash悬浮窗
-    WaitUntilRemove(() => document.getElementById('flowerContainerDiv'));
+    WaitUntilAction(() => document.getElementById('flowerContainerDiv'));
     //移除开通黄钻
-    WaitUntilRemove(() => document.getElementById('QM_Container_3').children[1]);
+    WaitUntilAction(() => document.getElementById('QM_Container_3').children[1]);
+    WaitUntilAction(() => document.getElementById('tb_vip_li'));
     //移除签到
-    WaitUntilRemove(() => document.getElementById('QM_Container_31'));
-    WaitUntilRemove(() => document.getElementById('QM_Container_100005'));
+    WaitUntilAction(() => document.getElementById('QM_Container_31'));
+    WaitUntilAction(() => document.getElementById('QM_Container_100005'));
     //移除天气
-    WaitUntilRemove(() => document.getElementById('weather-module'));
+    WaitUntilAction(() => document.getElementById('weatherDiv'));
     //移除小游戏
-    WaitUntilRemove(() => document.getElementById('leftMenu').children[1]);
+    WaitUntilAction(() => document.getElementById('leftMenu').children[1]);
+    //优化左侧tab栏
+    WaitUntilAction(() => document.getElementById('tab_menu_mv'));
+    WaitUntilAction(() => document.getElementById('tab_menu_class'));
+    WaitUntilAction(() => document.getElementById('tab_menu_app'));
+    //移除动态操作栏
+    WaitUntilAction(() => document.getElementById('feed_friend').children[0]);
+    //移除信息栏
+    WaitUntilAction(() => document.getElementById('headContainer').children[0]);
+    //移除大拇指
+    WaitUntilAction(() => document.getElementById('like_button_pannel'));
+    WaitUntilAction(() => document.getElementById('speed_signal_btn'));
+
+
+    WaitUntil(() => document.querySelectorAll('a[title="QQ空间独立版"]').length > 0, 100)
+        .then(() => {
+            document.querySelectorAll('a[title="QQ空间独立版"]')
+                    .forEach((value) => {
+                        value.remove();
+                    });
+        });
 }
 
 function sleep(ms) {
@@ -105,30 +149,44 @@ function Print(text) {
 }
 
 /**
- * 异步执行当某个元素出现时立马删除它
+ * 异步执行当某个元素出现时立马某个动作
  * @param {function} element 返回指定元素的函数
+ * @param {string} action 执行动作（可选click、remove、mouseover）
  * @param {number} step 每次检查间隔时间 ms
  * @param {number} timeOut 超时时间 ms
  * @returns {Promise<void>}
  * @constructor
  */
-async function WaitUntilRemove(element, step = 50, timeOut = 1000 * 30) {
+async function WaitUntilAction(element, action = 'remove', step = 50, timeOut = 1000 * 30) {
     let count = 0;
     let outCount = timeOut / step;
 
-    while (count <= outCount) {
-        count++;
-        await sleep(step);
-        try {
+    try {
+        while (count <= outCount) {
+            count++;
+            await sleep(step);
+
             let _c = element();
             if (_c != null) {
-                console.log('移除元素', _c);
-                _c.remove();
-                return;
+                if (action === 'click') {
+                    _c.click();
+                }
+                if (action === 'remove') {
+                    _c.remove();
+                }
+                if (action === 'mouseover') {
+                    _c.dispatchEvent(new MouseEvent('mouseover', {
+                        bubbles: true,  // 事件是否应该冒泡
+                        cancelable: true,  // 事件是否可以取消
+                        view: window,  // 事件的视图
+                    }));
+                }
+                break;
             }
-        } catch (e) {
 
         }
+    } catch (e) {
+
     }
 }
 
@@ -151,6 +209,15 @@ async function WaitUntil(control, step = 500, timeOut = 10000) {
         if (_c) {
             return;
         }
+    }
+}
+
+function openMain() {
+    let b = document.getElementById('ShuoShuoDeleteMain');
+    if (b.classList.contains('moving')) {
+        b.classList.remove('moving');
+    } else {
+        b.classList.add('moving');
     }
 }
 
@@ -313,3 +380,4 @@ async function NextPage(order) {
         ChangeStateText('未运行', 'red');
     }
 }
+
